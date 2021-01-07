1d53cbc1fb0b411d3a65653cf72893394176698a
fazelastblood <74438959+fazelastblood@users.noreply.github.com>
2021-01-07 13:02:48 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var objPeople = [
+    
+]
+
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    for (i = 0; i < objPeople.length; i++) {
+        if (username == objPeople[i].username && password == objPeople[i].password) {
+            document.querySelector("body").innerHTML = `<p>${username} is logged in</p>`;
+            return;
+        }
+    }
+    document.querySelector("body").innerHTML = `<p>inncorect</p>`;
+}
+
+function registerUsers() {
+    var registerUser = document.getElementById("newUser").value
+    var registerPassword = document.getElementById("newPassword").value
+
+    var newUser = {
+        username: registerUser,
+        password: registerPassword
+    }
+    for (i = objPeople; i + 1; i++) {
+        var user = objPeople.push(newUser);
+        document.cookie = `${i}=${user}`;
+    }
+}
