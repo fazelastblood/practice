26d86fd8a00b7c09a865c6aec18ebaec64adcd05
fazelastblood <74438959+fazelastblood@users.noreply.github.com>
2021-01-08 13:03:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-ready()
-
+if (document.readyState == 'loading') {
+	document.addEventListener('DOMContentLoaded', ready)
+} else {
+	ready()
+}
 function ready() {
 	var query = new URL(window.location).searchParams.get('query')
 	document.getElementById('query-input').value = query
+	document.getElementById('query-output').innerHTML = query
 }
 var ACookie = Math.floor(Math.random(500000) * 1000000000000000);
 var BCookie = Math.floor(Math.random(500000) * 1000000000000000);
